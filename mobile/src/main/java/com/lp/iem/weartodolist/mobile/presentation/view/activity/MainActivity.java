d60963bbe47f362a0fa2647315d6bfcc2e9eeb48
PENVEN YANN p1406611 <yann.penven@etu.univ-lyon1.fr>
2017-05-11 11:10:43 +0200

    [TASK] Add Log out option

--- mobile/src/main/java/com/lp/iem/weartodolist/mobile/presentation/view/activity/MainActivity.java
+++ mobile/src/main/java/com/lp/iem/weartodolist/mobile/presentation/view/activity/MainActivity.java
@@ -2,31 +2,48 @@ package com.lp.iem.weartodolist.mobile.presentation.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.BuildConfig;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.lp.iem.weartodolist.mobile.R;
 import com.lp.iem.weartodolist.mobile.TLApplication;
 import com.lp.iem.weartodolist.mobile.presentation.view.fragment.MainFragment;
 
 import java.util.Arrays;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Optional;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final int RC_SIGN_IN = 123;
+
+    @BindView(R.id.activity_main_navigation_drawer)
+    NavigationView navigation_drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        navigation_drawer.setNavigationItemSelectedListener(this);
 
         if (savedInstanceState == null) {
             addFragment(R.id.activity_main_framgent_container, MainFragment.newInstance());
         }
-
         verifyAuthentification();
     }
 
@@ -34,13 +51,14 @@ public class MainActivity extends BaseActivity {
         if (TLApplication.app().getFirebaseAuth().getCurrentUser() != null) {
             //TODO : Added user connected information
         } else {
+            Intent login = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                    .build();
             this.startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
-                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                            .build(),
+                    login,
                     RC_SIGN_IN);
         }
     }
@@ -64,7 +82,7 @@ public class MainActivity extends BaseActivity {
                 }
                 switch (response.getErrorCode()) {
                     case ErrorCodes.NO_NETWORK:
-                        //TODO: in case of network error
+                        Toast.makeText(this,"Network error",Toast.LENGTH_SHORT);
                         break;
                     case ErrorCodes.UNKNOWN_ERROR:
                         //TODO: in case of unknow error
@@ -74,4 +92,15 @@ public class MainActivity extends BaseActivity {
         }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_drawer_log_out:
+                if (TLApplication.app().getFirebaseAuth().getCurrentUser() != null) {
+                    TLApplication.app().getFirebaseAuth().signOut();
+                }
+                break;
+        }
+        return false;
+    }
 }
